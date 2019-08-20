e238a8ee59ab1ffec6d9987df97ab9c609e2dc9d
alec.zheng <aleczheng05@gmail.com>
2019-08-20 15:35:36 +0800

    fix docker script updated.sh

--- houserush/script/docker/updated.sh
+++ houserush/script/docker/updated.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+git checkout master
+git pull
 docker build -f ./updated.dockerfile --no-cache -t servicecomb-samples/updated .
+docker-compose build --no-cache --force-rm
+docker-compose up --force-recreate
