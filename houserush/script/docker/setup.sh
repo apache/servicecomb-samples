a91fadfbf6b9cea40800173a9675a585952899f5
alec.zheng <aleczheng05@gmail.com>
2019-08-26 11:32:40 +0800

    fix docker -compose bug

--- houserush/script/docker/setup.sh
+++ houserush/script/docker/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -f base.dockerfile -t servicecomb-samples/base .
-docker build -f mysql.dockerfile -t servicecomb-samples/mysql .
+docker build -f mysql.dockerfile -t servicecomb-samples/mysql ../database
 
