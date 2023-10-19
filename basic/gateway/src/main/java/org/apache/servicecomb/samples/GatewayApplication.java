8acf6af9eb4106a22abfac4659bff31c3fce0c2e
liubao68 <bismy@qq.com>
2023-10-19 16:16:56 +0800

    basic demo using latest version (#108)

--- basic/gateway/src/main/java/org/apache/servicecomb/samples/GatewayApplication.java
+++ basic/gateway/src/main/java/org/apache/servicecomb/samples/GatewayApplication.java
@@ -17,17 +17,18 @@
 
 package org.apache.servicecomb.samples;
 
-import org.apache.servicecomb.springboot2.starter.EnableServiceComb;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-@EnableServiceComb
 public class GatewayApplication {
   public static void main(String[] args) throws Exception {
     try {
-      new SpringApplicationBuilder().web(WebApplicationType.NONE).sources(GatewayApplication.class).run(args);
+      new SpringApplicationBuilder()
+          .web(WebApplicationType.NONE)
+          .sources(GatewayApplication.class)
+          .run(args);
     } catch (Exception e) {
       e.printStackTrace();
     }
